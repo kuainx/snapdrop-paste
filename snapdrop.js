118f7806882a8febca3635180f0c38fe7f24f0ef
KUAI <2542862169@qq.com>
2021-03-13 17:45:56 +0800

    Update snapdrop.js

--- snapdrop.js
+++ snapdrop.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         snapDrop paste
-// @version      0.1
-// @description  为snapDrop添加鼠标粘贴区域
+// @version      0.2
+// @description  为snapDrop添加鼠标粘贴按钮
 // @author       kuai
 // @match        https://snapdrop.net/
 // @grant        none
@@ -9,10 +9,26 @@
 
 (function () {
 	"use strict";
-	let input = document.createElement("input");
-	input.value = "在此进行黏贴";
-	input.addEventListener("input", () => {
-		input.value = "在此进行黏贴";
+	let a = document.createElement("a");
+	a.className = "icon-button";
+	a.innerHTML = "粘贴";
+	a.addEventListener("click", async () => {
+		let files = [];
+		const clipboardItems = await navigator.clipboard.read();
+		for (const clipboardItem of clipboardItems) {
+			for (const type of clipboardItem.types) {
+				const blob = await clipboardItem.getType(type);
+				files.push(new File([blob], "image.png", {type}));
+			}
+		}
+		const peers = document.querySelectorAll("x-peer");
+		if (files.length > 0 && peers.length === 1) {
+			Events.fire("files-selected", {
+				files: files,
+				to: $$("x-peer").id,
+			});
+		}
 	});
-	document.querySelector("header").appendChild(input);
+	document.querySelector("header").appendChild(a);
+
 })();
